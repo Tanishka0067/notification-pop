f470874a08539536db2e00b08d35e021d0d6e11e
Tanishka Agarwal <144484154+Tanishka0067@users.noreply.github.com>
2024-07-29 10:51:13 +0530

    workbox

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
+// next.config.mjs
+import withPWA from 'next-pwa';
 
+const nextConfig = {
+  reactStrictMode: true,
+  // other Next.js config options here
+};
 
-const nextConfig = {};
-
-export default nextConfig;
+export default withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+})(nextConfig);
